f00bcc46398f95393a0ce5d3c90a5c083cebe298
unknown <>
2021-01-07 20:00:11 +0100

    add array message_content

---
+++ skypewaddle.py
@@ -0,0 +1,39 @@
+# Python program to read
+# json file
+
+
+import json
+
+# Opening JSON file
+f = open('messages.json',"r",encoding='utf-8')
+
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+
+def extract_values(obj, key):
+    """Pull all values of specified key from nested JSON."""
+    arr = []
+
+    def extract(obj, arr, key):
+        """Recursively search for values of key in JSON tree."""
+        if isinstance(obj, dict):
+            for k, v in obj.items():
+                if isinstance(v, (dict, list)):
+                    extract(v, arr, key)
+                elif k == key:
+                    arr.append(v)
+        elif isinstance(obj, list):
+            for item in obj:
+                extract(item, arr, key)
+        return arr
+
+    results = extract(obj, arr, key)
+    return results
+
+#begin recursion
+message_content=extract_values(data, "content")
+print(message_content)
+
+# Closing file
+f.close()
