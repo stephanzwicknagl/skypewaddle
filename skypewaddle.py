f18bc676c188a47c7461634603714d4c35da702c
stephanzwicknagl <stephanzwicknagl@gmx.de>
2021-03-31 19:45:45 +0200

    creation of call-date array calls[0]

--- skypewaddle.py
+++ skypewaddle.py
@@ -2,9 +2,11 @@
 #import os
 
 import json
+import datetime
+
 
 # Opening JSON file
-f = open('messages.json',"r",encoding='utf-8')
+f = open('data/messages.json',"r",encoding='utf-8')
 
 # returns JSON object as
 # a dictionary
@@ -42,17 +44,52 @@ def extract_durations(obj):
             arr.append(duration)
     return arr, days/60/60/24
 
+def extract_call_date(obj, calls):
+
+    for item in obj:
+        if(is_call(item)):
+            j = extract_values(item, 'originalarrivaltime')
+            year  = int(j[0][0:4])
+            month = int(j[0][5:7])
+            day   = int(j[0][8:10])
+            calls[0].append(datetime.date(year, month, day))
+
+
+def is_call(obj):
+    if(extract_values(obj, "messagetype") == ['Event/Call']):
+        return True
+    return False
+
+def date_graph(dates):
+    
+        
+
 """extract message content into array"""
 #begin recursion
 message_content=extract_values(data, "content")
 
 """extract call durations"""
 durations, days = extract_durations(message_content)
-print(durations)
+#print(durations)
 print("Number of calls: ",len(durations))
 print("Total skype-time in days: ",days)
 print("That is " + str(round(days*24,2)) + " hours!")
 print("     or " + str(round(days*24*60)) + " minutes!")
 print("        " + str(round(days*24*60*60)) + " seconds!")
 # Closing file
+
+#initialize files
+dates =[]
+time =[]
+calls=[dates,time]
+
+
+
+#extract MessageList as array
+#message_content[0] is most recent message
+message_content = data['conversations'][0]['MessageList']
+
+
+extract_call_date(message_content, calls)
+
 f.close()
