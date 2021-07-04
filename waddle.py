7b19f2dd668c77d1cd181aaf8d00797022614115
stephanzwicknagl <stephanzwicknagl@gmx.de>
2021-07-05 01:29:12 +0200

    caller, terminator and fixed call id

--- waddle.py
+++ waddle.py
@@ -4,10 +4,13 @@ import json
 import datetime
 import numpy as np
 import pandas as pd
+from pandas.core.frame import DataFrame
 import plotly.express as px
 import plotly.graph_objects as go
 from scipy import signal
 import re
+# for my_timezone from location
+import pytz
 
 if not os.path.exists("images"):
     os.mkdir("images")
@@ -15,7 +18,7 @@ if not os.path.exists("images"):
 
 
 
-def get_calls(path):
+def get_calls(path, my_timezone):
     """ takes json file path as argument 
     open the json file and load into data
     messages = data['conversations'][0]['MessageList']  (this part should be manually selectable)
@@ -29,11 +32,13 @@ def get_calls(path):
     returns dataframe"""
     df = pd.DataFrame(columns=[
         'Call ID',
+        'ID',
         'Start Time',
         'End Time',
-        'Duration',
-        'Duration Skype'
-        'Weekday'])
+        'Duration'
+        'Weekday',
+        'Caller',
+        'Terminator'])
     df.set_index('Call ID', inplace=True)
 
     f = open(path, 'r', encoding='utf-8')
@@ -45,45 +50,56 @@ def get_calls(path):
         # not-calls are ignored
         if not is_call(obj):
             continue
-        calls = get_times(obj)
+        calls = get_times(obj, my_timezone)
         # if call is missed/etc. calls is empty and it is ignored
         if calls is None:
             continue 
         df = df.combine_first(calls)
-    
-    df = calculate_durations(df)
-    
+
+    df = fix_old_ids(df)
+
     return df
             
 
 
 
-def get_times(obj):
-    """ takes an object from json-messages-file 
+def get_times(obj, my_timezone): 
+    """ takes a part of the json file and analyzes it
+    
+    takes an object from json-messages-file 
     returns df call
     get call ID
         call[0] = call ID
+        call
     is call starting?
         call[1] = datetime
+        who called?
     is call ending?
         call[2] = datetime
+        who hung up?
     return call
     """
-    content = (extract_values(obj, 'content'))[0]
-    time = get_call_time(obj)
 
-    id = re.findall('callId=\\"(\S+)\\"', content)[0]
+    content = (extract_values(obj, 'content'))[0]
+    # get datetime of event
+    time = get_call_time(obj, my_timezone)
+    # get caller/terminator of call
+    val_from = extract_values(obj, 'from')[0]
+    # unique identifier for each call to match start and end later
+    call_id = re.findall('callId=\\"(\S+)\\"', content)[0]
+    # secondary id identifier for calls before mid 2019(?)
+    id_sec = extract_values(obj, 'id')[0]
 
     start_end = re.findall('type=\\"(\S+)\\"', content)[0]
     if start_end == 'started':
-        calls = pd.DataFrame(data={'Call ID': id,'Start Time': time, 'End Time': np.nan}, index=[id])
+        calls = pd.DataFrame(data={'Call ID': call_id, 'ID': id_sec, 'Start Time': time, 'End Time': np.nan, 'Caller': val_from}, index=[id])
     elif start_end == 'ended':
         duration = re.findall('<duration>([0-9.]+)</duration>', content)
         if len(duration) != 0:
             duration=float(duration[0])
         else:
             duration=0
-        calls = pd.DataFrame(data={'Call ID': id,'Start Time': np.nan, 'End Time': time, 'Duration Skype': duration}, index=[id])
+        calls = pd.DataFrame(data={'Call ID': call_id, 'ID': id_sec, 'Start Time': np.nan, 'End Time': time, 'Duration': duration, 'Terminator': val_from}, index=[id])
     else:
         return
     calls.set_index('Call ID', inplace=True)
@@ -116,7 +132,7 @@ def extract_values(obj, key):
     results = extract(obj, arr, key)
     return results
 
-def get_call_time(obj):
+def get_call_time(obj, my_timezone):
     time    =     (extract_values(obj, 'originalarrivaltime'))[0]
     year    =     int(time[0:4])
     month   =     int(time[5:7])
@@ -125,15 +141,37 @@ def get_call_time(obj):
     minute  =     int(time[14:16])
     second  =     int(time[17:19])
 
-    return datetime.datetime(year, month, day, hour, minute, second)
+    return datetime.datetime(year, month, day, hour, minute, second, tzinfo=datetime.timezone.utc).astimezone(pytz.timezone(my_timezone))
 
 def calculate_durations(df):
-    """ takes filled dataframe df 
+    """ calculates call duration from skype times
+
+    takes filled dataframe df 
     returns dataframe df filled with duration
-    calculates call duration from skype times """
+    """
+    
     for index, row in df.iterrows():
         duration = row['End Time'] - row['Start Time']
         df.loc[index, 'Duration'] = float(duration.seconds)
+    return df
+
+def fix_old_ids(df): 
+    """This function carries the information
+    from the end directive to the correct call id
+    
+    some end call directives do not referernce the call id, but the 
+    message id of the start directive. 
+    End Time, Duration, Terminator is carried over to the correct call 
+    ID and wrong index is dropped
+    """
+    
+    for index, row in df.iterrows():
+        if len(index) <= 13:
+            df.loc[df['ID'] == index, 'End Time'] = df.loc[index,'End Time']
+            df.loc[df['ID'] == index, 'Duration'] = df.loc[index, 'Duration']
+            df.loc[df['ID'] == index, 'Terminator'] = df.loc[index, 'Terminator']
+            df.drop(index, inplace=True)
+
     return df
 
 def main():
@@ -145,7 +183,7 @@ def main():
             Duration
             Weekday 
     """
-    df = get_calls(path="data/31-03.json")
+    df = get_calls(path="data/04-07-2021.json", my_timezone="Europe/Berlin")
     print(df)
     """ save df as csv df.to_csv() """
     df.to_csv("data/dataframe.csv")
